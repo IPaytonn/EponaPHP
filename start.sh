ebc440118ebc652041f6e14b36d2e4213d0f8c1c
I_Is_Payton_ <IPaytonn@users.noreply.github.com>
2014-03-03 11:54:17 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ cd "$DIR"
 if [ -f ./bin/php5/bin/php ]; then
 	exec ./bin/php5/bin/php -d enable_dl=On EponaPHP.php $@
 else
-	echo "Please use the correct PHP bianaries."
+	echo "Please make sure you have the php bianaries (Found with your EponaPHP download.)"
 	exec php -d enable_dl=On EponaPHP.php $@
 fi
