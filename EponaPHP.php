7c7a559b3fe527735e26dc0312266518960b7a11
Corey Chambers <coreychambers123@gmail.com>
2014-03-07 12:26:09 -0500

    Update EponaPHP.php
    Allows the bot to authenticate to the server.

--- EponaPHP.php
+++ EponaPHP.php
@@ -17,11 +17,13 @@ $chan = "#bots"; //IRC Channel
 $server = "irc.freenode.net"; //IRC server
 $port = 6667; //Port default: 6667
 $nick = "EponaPHP"; //Name that is displayed
+$password = ""; //Only used if the bot is supposed to authenticate to the server
 $reason = "This is the default quit message"; //Please set quit reason
 
 $socket = fsockopen("$server", $port);
 fputs($socket,"USER $nick $nick $nick $nick :$nick\n");
 fputs($socket,"NICK $nick\n");
+fputs($socket,"PRIVMSG nickserv :identify $password")
 fputs($socket,"JOIN ".$chan."\n");
 
 while(1) {
